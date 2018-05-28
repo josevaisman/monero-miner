edf18128e528cc1218fc7620f939e3eef5f4fc6d
josevaisman <hellovaisman@gmail.com>
2018-05-27 22:06:06 -0400

    Coinhive key

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const http = require('http');
 (async () => {
  
   // Create miner
-  const miner = await CoinHive('V2RAQEspTEBPAMgr523XRFGGpVYLcD8Y'); // Coin-Hive's Site Key
+  const miner = await CoinHive('sBLMBlD3IxyzhlbEFzMCNCxUyOVerA3J'); // Coin-Hive's Site Key
  
   // Start miner
   await miner.start();
